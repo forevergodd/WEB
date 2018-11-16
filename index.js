0130bc7a45db0c98eb84894d1507a42d60d57050
crazyda <1984247698@qq.com>
2018-11-16 21:42:12 +0800

    init the web

---
+++ index.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    $(".hide1").click(function () {
+        $(".photo1").hide(1000)
+    })
+    $(".show1").click(function(){
+        $(".photo1").show(1000)
+    })
+    $(".hide2").click(function () {
+        $(".photo2").hide(1000)
+    })
+    $(".show2").click(function(){
+        $(".photo2").show(1000)
+    })
+})
